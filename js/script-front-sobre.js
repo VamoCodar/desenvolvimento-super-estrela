904e532fd887a1793a2b3593872c0893f05fc1e6
EuSouAlaska <51723157+EuSouAlaska@users.noreply.github.com>
2020-12-07 23:14:02 -0300

    finalizado todas as paginas

--- js/script-front-sobre.js
+++ js/script-front-sobre.js
@@ -58,34 +58,6 @@ function targetMenu(event) {
   }
 }
 
-//carousel
-//iniializa carousel porte gratis
-function owlInitialize3() {
-  if ($(window).width() >= 960) {
-    $(".relacionados-container").slick("unslick");
-  } else {
-    $(".relacionados-container").slick({
-      dots: false,
-      infinite: true,
-      slidesToShow: 1,
-      centerMode: false,
-      centerPadding: "0px",
-      arrows: false,
-      variableWidth: true,
-      slidesToScroll: 1,
-    });
-  }
-}
-
-//só tem carousel apartir de tal tamanho
-//receitas
-$(document).ready(function () {
-  owlInitialize3();
-});
-$(window).resize(function () {
-  owlInitialize3();
-});
-
 body.addEventListener("click", targetMenu);
 botaoMenuMobile.addEventListener("click", abreMenu);
 window.addEventListener("resize", resize);
